9e56b4c6fb168c6578729119373becde0fa80dd2
aniketvasadkar <aniketvasadkarofficial@gmail.com>
2020-04-25 15:18:53 +0530

    converted Spring boot project to plain maven project & added camel core jar. Created simple route for file cut paste

--- CamelPoc/src/main/java/com/poc/camel/CamelPoc/CamelPocApplication.java
+++ CamelPoc/src/main/java/com/poc/camel/CamelPoc/CamelPocApplication.java
@@ -1,13 +1,23 @@
 package com.poc.camel.CamelPoc;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
 
-@SpringBootApplication
 public class CamelPocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CamelPocApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		CamelContext camelContext = new DefaultCamelContext();
+		camelContext.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("file:input/").to("file:output/");
+			}
+		});
+
+		camelContext.start();
+		Thread.sleep(10000);
+		camelContext.stop();
 	}
 
 }
--- CamelPoc/src/test/java/com/poc/camel/CamelPoc/CamelPocApplicationTests.java
+++ CamelPoc/src/test/java/com/poc/camel/CamelPoc/CamelPocApplicationTests.java
@@ -1,12 +1,6 @@
 package com.poc.camel.CamelPoc;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class CamelPocApplicationTests {
-
-	@Test
 	void contextLoads() {
 	}
 
