6248d35e108bee36beb911644e131032a65946da
aniketvasadkar <aniketvasadkarofficial@gmail.com>
2020-04-25 14:17:13 +0530

    Committing blank spring boot camel project need to remove spring camel starter dependency and add core camel

---
+++ CamelPoc/src/test/java/com/poc/camel/CamelPoc/CamelPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.camel.CamelPoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
